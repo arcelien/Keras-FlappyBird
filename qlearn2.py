4c75d91febcbf1e5e6e9c63637bc64ebe37ca325
Veerapaneni <veerapaneni1@merryweather.llnl.gov>
2017-08-01 15:54:51 -0700

    6 dimensional input: absolution position of self and lower and upper pipe

--- qlearn2.py
+++ qlearn2.py
@@ -23,12 +23,15 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD , Adam
 import tensorflow as tf
 
+import time
+import random
+
 GAME = 'bird' # the name of the game being played for log files
 CONFIG = 'nothreshold'
 ACTIONS = 2 # number of valid actions
 GAMMA = 0.99 # decay rate of past observations
-OBSERVATION = 3200. # timesteps to observe before training
-EXPLORE = 3000000. # frames over which to anneal epsilon
+OBSERVATION = 1000. # timesteps to observe before training
+EXPLORE = 500000. # frames over which to anneal epsilon
 FINAL_EPSILON = 0.0001 # final value of epsilon
 INITIAL_EPSILON = 0.1 # starting value of epsilon
 REPLAY_MEMORY = 50000 # number of previous transitions to remember
@@ -59,13 +62,16 @@ img_channels = 4 #We stack 4 frames
 #     print("We finish building the model")
 #     return model
 
+aFrameInputDim = 6
 def buildmodel():
     print("Now we build the model")
     model = Sequential()
-    model.add(Dense(9, input_shape=(8,)))
+    model.add(Dense(1+aFrameInputDim*4, input_shape=(4,aFrameInputDim))) #Was 4 by two
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.1))
+    model.add(Dense(150))
     model.add(Activation('relu'))
-    model.add(Dense(300))
-    model.add(Activation('relu'))
+    model.add(Flatten())
     model.add(Dense(2))
 
     adam = Adam(lr=LEARNING_RATE)
@@ -95,8 +101,9 @@ def trainNetwork(model,args):
     #In Keras, need to reshape
     # s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])  #1*80*80*4
 
-    s_t = np.array(x_t*4)
-    s_t = np.reshape(s_t, (1, 8))
+    s_t = np.array([x_t, x_t, x_t, x_t])
+    s_t = s_t.reshape(1, 4, aFrameInputDim)
+    # s_t = np.reshape(s_t, (1, 8))
     
 
     if args['mode'] == 'Run':
@@ -122,8 +129,16 @@ def trainNetwork(model,args):
         if t % FRAME_PER_ACTION == 0:
             if random.random() <= epsilon:
                 print("----------Random Action----------")
-                action_index = random.randrange(ACTIONS)
-                a_t[action_index] = 1
+                #action_index = random.randrange(ACTIONS)
+
+                if random.uniform(0,1) < 0.8:
+                    action_index = 0
+                    a_t[0] = 1
+                else:
+                    action_index = 1
+                    a_t[1] = 1
+
+                # a_t[action_index] = 1
             else:
                 q = model.predict(s_t)       #input a stack of 4 images, get the prediction
                 max_Q = np.argmax(q)
@@ -138,15 +153,18 @@ def trainNetwork(model,args):
         x_t1, r_t, terminal = game_state.frame_step(a_t)
 
         # x_t1 = skimage.color.rgb2gray(x_t1_colored)
-        # x_t1 = skimage.transform.resize(x_t1,(80,80))
+        # x_t1 = skimage.transform.resize(x_t1,(80,80))0
         # x_t1 = skimage.exposure.rescale_intensity(x_t1, out_range=(0, 255))
 
         # x_t1 = x_t1.reshape(1, x_t1.shape[0], x_t1.shape[1], 1) #1x80x80x1
         # s_t1 = np.append(x_t1, s_t[:, :, :, :3], axis=3)
 
-        s_t1 = np.array(s_t[0][2:])
-        s_t1 = np.concatenate((s_t1, np.array(x_t1)), axis=0)
-        s_t1 = np.reshape(s_t1, (1, 8))
+        s_t1 = s_t[0][:3]
+        # print(s_t1)
+        # print(np.array([x_t1]))
+        s_t1 = np.concatenate((s_t1, np.array([x_t1])))
+        # print(s_t1)
+        s_t1 = s_t1.reshape(1, 4, aFrameInputDim)
 
         # store the transition in D
         D.append((s_t, action_index, r_t, s_t1, terminal))
@@ -160,9 +178,11 @@ def trainNetwork(model,args):
 
 
 
-            inputs = np.zeros((BATCH, s_t.shape[1], s_t.shape[2], s_t.shape[3]))   #32, 80, 80, 4
-            print (inputs.shape)
-            targets = np.zeros((inputs.shape[0], ACTIONS))                         #32, 2
+            # inputs = np.zeros((BATCH, s_t.shape[0][1], s_t.shape[0][2], s_t.shape[0][3]))   #32, 80, 80, 4
+            # # print(inputs.shape)
+            # targets = np.zeros((inputs.shape[0], ACTIONS))                         #32, 2
+            inputs = np.zeros((BATCH,4,aFrameInputDim))
+            targets = np.zeros((BATCH,ACTIONS))
 
             #Now we do the experience replay
             for i in range(0, len(minibatch)):
@@ -205,9 +225,10 @@ def trainNetwork(model,args):
         else:
             state = "train"
 
-        print("TIMESTEP", t, "/ STATE", state, \
-            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
-            "/ Q_MAX " , np.max(Q_sa), "/ Loss ", loss)
+        if t%20 == 0:
+            print("TIMESTEP", t, "/ STATE", state, \
+                "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
+                "/ Q_MAX " , np.max(Q_sa), "/ Loss ", loss)
 
     print("Episode finished!")
     print("************************")
